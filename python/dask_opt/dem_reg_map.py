db19e58cb4fb303f5204f24a10f3f8c0f85dadb5
Your Name <stefansim2000@gmail.com>
2025-10-16 10:49:45 +0200

    refactor demmap_pos to use dask

--- python/dask_opt/dem_reg_map.py
+++ python/dask_opt/dem_reg_map.py
@@ -1,61 +1,36 @@
 import numpy as np
 
-def dem_reg_map(sigmaa,sigmab,U,W,data,err,reg_tweak,nmu=500):
-    """
-    dem_reg_map
-    computes the regularization parameter
-    
-    Inputs
+def dem_reg_map(sigmaa, sigmab, U, W, data, err, reg_tweak, nmu=500):
+    sa2 = np.asarray(sigmaa, dtype=np.float64)**2
+    sb2 = np.asarray(sigmab, dtype=np.float64)**2
+    U = np.asarray(U, dtype=np.float64)
+    data = np.asarray(data, dtype=np.float64)
+    err = np.asarray(err, dtype=np.float64)
 
-    sigmaa: 
-        gsv vector
-    sigmab: 
-        gsv vector
-    U:      
-        gsvd matrix
-    V:      
-        gsvd matrix
-    data:   
-        dn data
-    err:    
-        dn error
-    reg_tweak: 
-        how much to adjust the chisq each iteration
+    nf = sa2.shape[0]
+    if U.shape[0] != nf:
+        raise ValueError("U shape incompatible with sigmas")
 
-    Outputs
+    with np.errstate(divide="ignore", invalid="ignore"):
+        sigs = np.where(sigmab != 0, np.asarray(sigmaa, float)/np.asarray(sigmab, float), np.nan)
+    sigs = sigs[np.isfinite(sigs) & (sigs > 0)]
+    if sigs.size == 0:
+        minx, maxx = 1e-15, 1e15
+    else:
+        smax = np.max(sigs)
+        smin = max(np.min(sigs), np.finfo(float).tiny)
+        minx = max(smax*1e-15, np.finfo(float).tiny)
+        maxx = max(smax*1e+15, minx*10)
 
-    opt:
-        regularization paramater
+    mu = np.geomspace(minx, maxx, max(2, int(nmu)))
 
-    """
- 
+    coef = (U @ data).reshape(nf, 1)
+    mu_row = mu.reshape(1, -1)
 
-    nf=data.shape[0]
-    nreg=sigmaa.shape[0]
+    num = mu_row * sb2.reshape(-1, 1) * coef
+    den = sa2.reshape(-1, 1) + mu_row * sb2.reshape(-1, 1)
+    frac2 = (num / den)**2
 
-    arg=np.zeros([nreg,nmu])
-    discr=np.zeros([nmu])
-
-    sigs=sigmaa[:nf]/sigmab[:nf]
-    maxx=max(sigs)
-    # minx=min(sigs)**2.0*1E-2
-    ## Useful to make the lower limit smaller?
-    minx=min(sigs)**2.0*1E-4
-
-    ## Range from original non-map code
-    # maxx=max(sigs)*1E3
-    # minx=max(sigs)*1E-15
-
-    step=(np.log(maxx)-np.log(minx))/(nmu-1.)
-    mu=np.exp(np.arange(nmu)*step)*minx
-    for kk in np.arange(nf):
-        coef=data@U[kk,:]
-        for ii in np.arange(nmu):
-            arg[kk,ii]=(mu[ii]*sigmab[kk]**2*coef/(sigmaa[kk]**2+mu[ii]*sigmab[kk]**2))**2
-    
-    discr=np.sum(arg,axis=0)-np.sum(err**2)*reg_tweak
-  
-    opt=mu[np.argmin(np.abs(discr))]
-    # print(opt)
-
-    return opt
+    discr = np.sum(frac2, axis=0) - np.sum(err**2) * float(reg_tweak)
+    opt = mu[np.argmin(np.abs(discr))]
+    return float(opt)
